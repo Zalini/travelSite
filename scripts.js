69961445453e7f9742d49c99765ff9b8c9cbc0b7
Zalini P <zalininyc@gmail.com>
2016-02-26 15:29:40 -0500

    content

--- scripts.js
+++ scripts.js
@@ -1,20 +1,14 @@
 $(document).ready(function() {
-
 //click the image and the corresponding blurb slides in
 	$("#pics img").on("click", function(){
 	$("#contentBox, .blurb").show().velocity({ translateX: [ 0, 300 ] },
 	  { duration: 1000 });
-
 
 //hide list and articles
 	$(".titles").hide();
 	$("#arrow").show();
 	$("#articlesCarousel").hide();
 	$(".mobileTitles").hide();
-
-//image slides in from the left
-	$(this).velocity({ translateX: [ 200, 0 ] },
-	  { duration: 1000 });
 
 //hide the siblings of this particular image
 	$(this).siblings("img").hide();
@@ -27,15 +21,15 @@ $(document).ready(function() {
 	});
 
 
-	$(".articlesCarousel").slick({
-		  dots: true,
-		  infinite: true,
-		  fade: true,
-		  autoPlay: true,
-		  autoplaySpeed: 1000,
-		  arrows: true,
-		});
-
+$(".articlesCarousel").slick({
+	dots: true,
+	infinite: true,
+	fade: true,
+	autoPlay: true,
+	autoplaySpeed: 1000,
+	arrows: true,
+	swipe: true
+});
 
 $("#upArrow p").click(function(){
 	$(window).scrollTop(0);
@@ -43,8 +37,6 @@ $("#upArrow p").click(function(){
 
 
 /***********For the email and name confirmation***********/
-var nameTest = false;
-var emailTest = false;
 $("#emailConfirmBox").hide();
 
 $("#contactName").focusout(function(){
@@ -55,32 +47,28 @@ $("#emailInfo").focusout(function(){
 	checkEmail();
 });
 
-
 function checkName(){
 	var contactNameLength = $("#contactName").val().length;
+	var contactName = $("#contactName").val();
 	if(contactNameLength > 1){
-		nameTest = true;
+		$("#alertText").text("You're awesome " + contactName + " !");
 	}
 } 
 
-
 function checkEmail(){
 	var emailLength = $("#emailInfo").val().length;
-	if(emailLength < 6 || emailLength >15){
+	if(emailLength < 6){
 		$("#alertText").text("Please type in a valid email address");
-		emailTest = true;
 	} else {
 		$("#alertText").hide();
 	}
 } 
 
-
 $("form").submit(function(event){
 	checkName();
 	checkEmail();
 	var contactName = $("#contactName").val();
-	$("#emailConfirmBox h1").text("thank you"+ " " + contactName + " " + "for subscribing!");
-	// alert("Thank you" + ' ' + contactName);
+	$("#emailConfirmBox h1").text("Thank you"+ " " + contactName + " " + "for subscribing!");
 	$("#emailConfirmBox").show();
 	event.preventDefault();
 });
@@ -89,23 +77,4 @@ $("#exitBtn").on("click", function(){
 	$("#emailConfirmBox").hide();
 });
 
-// $("#submitBtn").on("click", function(){
-// 	var nameInfoBox = $("contactName").val();
-// 	var emailInfoBox = $("emailInfo").val();
-// 	if(nameInfoBox && emailInfoBox == ""){
-// 		$("#alertText").html("missing text");
-// 	}
-
-// });
-
-// if(errorName && errorEmail == false){
-// 	alert("Yippie, great news to come your way!")
-// }
-
-
-
 });
-
-
-
-
